38a0e97ac34d98ea81c5b9501375b916572af10d
siweiwang <wangsiwei2003@hotmail.com>
2016-09-07 00:43:35 -0400

    partial application

---
+++ PartialApplication.rb
@@ -0,0 +1,3 @@
+# Partial function application is the ability to take a function of many parameters and apply arguments to some of the parameters to create a new function that needs only the application of the remaining argument to produce the equivalent of applying all argument to the original functions
+
+# given f( )
